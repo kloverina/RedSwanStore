234107418b7dfc5773ef038474e48f04a0e0422a
Daniil Zabaluev <elgoogecaf@gmail.com>
2021-05-20 20:18:40 +0300

    Create Language model

---
+++ RedSwanStore/Data/Models/Language.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedSwanStore.Data.Models
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+        
+        [Required]
+        public bool Voice { get; set; }
+        
+        [Required]
+        public bool Text { get; set; }
+    }
+}
