4f0de53b8f52f65de5975a3d1b38990f1b808060
Daniil Zabaluev <elgoogecaf@gmail.com>
2021-05-20 20:26:25 +0300

    Create Os model

---
+++ RedSwanStore/Data/Models/Os.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedSwanStore.Data.Models
+{
+    public class Os
+    {
+        public int Id { get; set;}
+        
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+        
+        [StringLength(50)]
+        public string ExtraInfo { get; set; }
+    }
+}
