0e3bb2375a2b8a5dc50109e24b50eb029f77ef65
Daniil Zabaluev <elgoogecaf@gmail.com>
2021-05-21 02:24:19 +0300

    Install packages and configure app DB context

---
+++ RedSwanStore/Data/RedSwanStoreDBContent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedSwanStore.Data.Models;
+
+namespace RedSwanStore.Data
+{
+    public class RedSwanStoreDBContent : DbContext
+    {
+        public RedSwanStoreDBContent(DbContextOptions<RedSwanStoreDBContent> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GameInfo> GameInfos { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Os> Oses { get; set; }
+        public DbSet<GameSystemRequirement> GameSystemRequirements { get; set; }
+        public DbSet<GameMedia> GameMedias { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserLibrary> UserLibraries { get; set; }
+        public DbSet<UserLibraryGame> UserLibraryGames { get; set; }
+    }
+}
--- RedSwanStore/Startup.cs
+++ RedSwanStore/Startup.cs
@@ -6,20 +6,38 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using RedSwanStore.Data;
 
 namespace RedSwanStore
 {
     public class Startup
     {
+        private IConfigurationRoot dbConfig;
+
+
+        public Startup(IWebHostEnvironment hostEnv)
+        {
+            dbConfig = new ConfigurationBuilder()
+                .SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json")
+                .Build();
+        }
+        
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RedSwanStoreDBContent>(
+                options => options.UseSqlServer(dbConfig.GetConnectionString("DefaultConnection"))
+            );
             services.AddControllersWithViews();
         }
 
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
