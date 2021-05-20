be4464f1276c13d55b59ca413839e0c270c2e8dc
Daniil Zabaluev <elgoogecaf@gmail.com>
2021-05-21 00:44:42 +0300

    Create User model

---
+++ RedSwanStore/Data/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedSwanStore.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string UserId { get; set; }
+        
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+        
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Surname { get; set; }
+        
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        public string Login { get; set; }
+        
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+        
+        [Required]
+        public string Photo { get; set; }
+    }
+}
