6a183e26b88febeb7bd5845aa4d4b92bbb7e98d5
Daniil Zabaluev <elgoogecaf@gmail.com>
2021-05-21 15:19:54 +0300

    Add PriceCategory model

---
+++ RedSwanStore/Data/Models/PriceCategory.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedSwanStore.Data.Models
+{
+    public class PriceCategory
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        public string Name { get; set; }
+        
+        [Required]
+        public decimal MinPrice { get; set; }
+        
+        [Required]
+        public decimal MaxPrice { get; set; }
+    }
+}
