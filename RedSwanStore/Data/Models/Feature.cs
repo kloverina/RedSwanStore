62d53a58ffabf637b621f0c93337c03f2297afd2
Daniil Zabaluev <elgoogecaf@gmail.com>
2021-05-21 14:56:55 +0300

    Add Feature and Genre models

---
+++ RedSwanStore/Data/Models/Feature.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedSwanStore.Data.Models
+{
+    public class Feature
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        public string Name { get; set; }
+    }
+}
