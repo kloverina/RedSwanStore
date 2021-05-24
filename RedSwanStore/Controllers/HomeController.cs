65e5ede4121cdba904bb6dabd43122027972e779
Daniil Zabaluev <elgoogecaf@gmail.com>
2021-05-25 01:32:13 +0300

    Deleted all shit I occasionally commited

--- RedSwanStore/Controllers/HomeController.cs
+++ RedSwanStore/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using RedSwanStore.Data.Interfaces;
-using RedSwanStore.Data.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace RedSwanStore.Controllers
 {
@@ -11,36 +7,10 @@ namespace RedSwanStore.Controllers
     [Route("home")]
     public class HomeController : Controller
     {
-        private readonly IGameRepo gamesTable;
-        private readonly IFeatureRepo featuresTable;
-        private readonly IGenreRepo genresTable;
-        private readonly IPriceCategoryRepo categoryTable;
-
-        public HomeController(IGameRepo gamesTable, IFeatureRepo featuresTable, IGenreRepo genresTable, IPriceCategoryRepo categoryTable)
-        {
-            this.gamesTable = gamesTable;
-            this.featuresTable = featuresTable;
-            this.genresTable = genresTable;
-            this.categoryTable = categoryTable;
-        }
-        
         [Route("")]
         public ViewResult Index()
         {
-            var filter = new GameFilter {
- 
-            };
-
-            var priceCategories = new List<PriceCategory> {
-                categoryTable.GetCategoryByName("Цена для галочки"),
-                categoryTable.GetCategoryByName("Ниже среднего"),
-                categoryTable.GetCategoryByName("Очень многа")
-            };
-            
-            var allGames = gamesTable.GetGamesByFilter(filter, priceCategories);
-            var game = new List<Game> {gamesTable.GetGameByUrl("evans-remains") };
-            
-            return View(allGames);
+            return View();
         }
     }
 }
