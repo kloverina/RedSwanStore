33acd65f4394fe95565c825298ce666c9db9e44a
Daniil Zabaluev <elgoogecaf@gmail.com>
2021-05-25 01:24:10 +0300

    Add repo for GameInfo table
    Add method to check and update discounts for the game every day at 18.00. I've not really tested it so I don't even know if this method works :)

---
+++ RedSwanStore/Data/Interfaces/IGameInfoRepo.cs
@@ -0,0 +1,14 @@
+﻿namespace RedSwanStore.Data.Interfaces
+{
+    /// <summary>
+    /// An interface to work with database GameInfo table.
+    /// </summary>
+    public interface IGameInfoRepo
+    {
+        /// <summary>
+        /// Check GameInfo table every day at 18.00 and clear discount for the game
+        /// which discount end date has come.
+        /// </summary>
+        public void CheckAndUpdateDiscounts();
+    }
+}
---
+++ RedSwanStore/Data/Repositories/GameInfoRepo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RedSwanStore.Data.Interfaces;
+using RedSwanStore.Data.Models;
+
+namespace RedSwanStore.Data.Repositories
+{
+    /// <summary>
+    /// The class to work with database GameInfo table.
+    /// </summary>
+    public class GameInfoRepo : IGameInfoRepo
+    {
+        private readonly RedSwanStoreDBContent dbContent;
+
+        public GameInfoRepo(RedSwanStoreDBContent dbContent)
+        {
+            this.dbContent = dbContent;
+        }
+        
+        public void CheckAndUpdateDiscounts()
+        {
+            DateTime checkTime = new DateTime(
+                DateTime.Now.Year, 
+                DateTime.Now.Month, 
+                DateTime.Now.Day, 
+                18, 
+                0, 
+                0
+            );
+            
+            if (DateTime.Now < checkTime)
+                return;
+
+            IEnumerable<GameInfo> updatedInfos = dbContent.GameInfos
+                .Where(gi => gi.DiscountEndDate != DateTime.MinValue && gi.DiscountEndDate <= DateTime.Now);
+
+            foreach (GameInfo updatedInfo in updatedInfos)
+            {
+                updatedInfo.Discount = 0.00f;
+                updatedInfo.DiscountEndDate = DateTime.MinValue;
+            }
+            
+            dbContent.GameInfos.UpdateRange(updatedInfos);
+            dbContent.SaveChanges();
+        }
+    }
+}
