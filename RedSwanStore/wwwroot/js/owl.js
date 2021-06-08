812687455fa4df6f9a7599acf6e11af8a997232f
Kloverina <kloverina@yandex.ru>
2021-06-08 22:17:55 +0300

    Game card update (added some media)

---
+++ RedSwanStore/wwwroot/js/owl.js
@@ -0,0 +1,20 @@
+
+
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+        items: 1,
+        loop: true,
+        lazyLoad:false,
+        margin: 10,
+        merge: false,
+        center:true,
+        nav: false,
+        video: true,
+        videoWidth: false,
+        videoHeight: false
+    });
+});
+
+
